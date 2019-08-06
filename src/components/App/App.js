64f9368b72bbc174e59b6a25959a8458b9481a62
qrr <qrr@kmd.dk>
2019-08-06 12:03:48 +0200

    First React App - initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from '../Home/Home';
+import Header from '../elements/Header/Header';
+import NotFound from '../elements/NotFound/NotFound';
+import Movie from '../Movie/Movie';
+
+
+const App = () => {
+
+    return(    
+    <div>
+        <BrowserRouter>
+        <React.Fragment>
+        <Header />
+        <Switch>
+            <Route path="/" component={Home} exact/>
+            <Route path="/:movieId" component={Movie} exact />
+            <Route component={NotFound} />
+        </Switch>
+        
+
+        </React.Fragment>
+        </BrowserRouter>
+
+    </div>
+    );
+
+
+}
+
+export default App;
